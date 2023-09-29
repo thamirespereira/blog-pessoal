02dc6b184dd07a70844a615fa18c6c9845534963
thamirespereira <thamiresfrpereira@hotmail.com>
2023-09-29 14:05:46 -0300

    conexao com banco de dados

---
+++ src/main/java/com/example/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,12 @@
+package com.example.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.blogpessoal.model.Postagem;
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+}
