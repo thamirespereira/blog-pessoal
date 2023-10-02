a1311410750f35be0bb68be6dd547b6d5ae8447c
thamirespereira <thamiresfrpereira@hotmail.com>
2023-10-02 15:53:18 -0300

    crud blog pessoal

--- src/main/java/com/example/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/example/blogpessoal/repository/PostagemRepository.java
@@ -1,12 +1,15 @@
 package com.example.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.blogpessoal.model.Postagem;
 
 
-@Repository
+
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
+	List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
